fe6aac6e6439ffd672f15672f368f25c110ae62d
amanpreetchatha <amanpreetchatha99@gmail.com>
2025-12-28 14:24:59 +0530

    working feature/add-customer

--- app/add-customer.tsx
+++ app/add-customer.tsx
@@ -1,18 +1,42 @@
-import { View } from "react-native";
-import { useUserContext } from "./providers/context";
-import { Button, Input } from "react-native-elements";
-import styles from "./styles";
-import { useState } from "react";
+import { supabase } from "@/utils/supabase";
 import { router } from "expo-router";
+import { useState } from "react";
+import { Alert, View } from "react-native";
+import { Button, Input, Text } from "react-native-elements";
+import { useUserContext } from "./providers/context";
+import styles from "./styles";
 
 export default function AddCustomer(){
     const user=useUserContext();
     const [name,setName]=useState("");
     const [phone,setPhone]=useState("");
     const [note,setNote]=useState("");
+    const [dataSubmitted, setDataSubmitted] = useState(false);
+    const [message,setMessage] = useState("");
 
-    function handleSubmit(){
-        console.log('works fine')
+    function validateData(){
+        setMessage("");
+        if((name!=="")&&(note!=="")){
+            handleSubmit();
+        }
+        else
+            setMessage("Name and Notes can't be empty");
+    }
+    async function handleSubmit(){
+
+        const customerData={"name": name, "phone": phone, "note": note, "last_updated": new Date()};
+        const { data, error, status } = await supabase
+        .from('customer_list')
+        .insert(customerData)
+        .single()
+        
+        if (error && status !== 406) {
+            setMessage(error.message);
+        }else
+        if(error)     
+            setMessage(error.message)
+        else
+            setMessage("Submitted!");
     }
 
     return (
@@ -33,12 +57,14 @@ export default function AddCustomer(){
                 onChangeText={(text)=>(setNote(text))}
             />
             <View style={styles.verticallySpaced}>
-                <Button title="Add" onPress={handleSubmit}/>
+                <Button title="Add" onPress={validateData}/>
             </View>
             <View style={styles.verticallySpaced}>
-                <Button title="Cancel" onPress={()=>router.push("/dashboard")}/>
+                <Button title="Cancel" onPress={()=>router.push("/")}/>
+            </View>
+            <View style={styles.verticallySpaced}>
+                <Text style={styles.text}>{ message }</Text>
             </View>
         </View>
     );
-
 }
