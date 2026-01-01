658f16f694b2f30fcc52a84dfbf209ba3aa319bc
amanpreetchatha <amanpreetchatha99@gmail.com>
2026-01-01 15:58:25 +0530

    testing on physical device

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,10 +3,13 @@ import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient, processLock } from '@supabase/supabase-js'
 
+const EXPO_PUBLIC_SUPABASE_URL="https://pnudfhiovsnzlyfsjmha.supabase.co";
+const EXPO_PUBLIC_SUPABASE_KEY="sb_publishable_ti-rn-Ht_Q2BwKzs7Vypng_2JuEHevY";
+
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
-  {
+  EXPO_PUBLIC_SUPABASE_URL,
+  EXPO_PUBLIC_SUPABASE_KEY,
+{
     auth: {
       storage: AsyncStorage,
       autoRefreshToken: true,
